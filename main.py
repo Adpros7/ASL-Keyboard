42de47a46c2f9e76cc9ccfe07575240550f565ac
Adpros7 <pranit.advik@gmail.com>
2026-07-30 19:05:29 -0400

    add American-SIgn-Language repo for live  detection

--- main.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import asl_alphabet.app as asl
-
-rec = asl.Recognizer()
-
-# Single image
-image = cv2.imread("img.jpg")
-letter, confidence, _ = rec.predict(image)
-print(f"Letter: {letter}, Confidence: {confidence:.2%}")
